df80cbdbf9a7df230ad2e99ac37cbb2054e1cb02
Isaac Familoni <91315160+isaacfib@users.noreply.github.com>
2025-02-28 19:37:41 +0100

    Update index.js
    code_1.0.0

---
+++ src/components/ui/BackgroundElements/index.js
@@ -0,0 +1,9 @@
+// nextrole_landing/src/components/ui/BackgroundElements/index.js
+export {
+  VerticalLinesGrid,
+  HorizontalLinesGrid,
+  BlurredBackgroundShape,
+  LayeredCircles,
+  GradientWashEllipse,
+  DecorativeDot,
+} from "./DecorationElements";
