9b080fee2b9a29294fd9f777822d6f347d611648
Isaac Familoni <91315160+isaacfib@users.noreply.github.com>
2025-03-01 06:18:17 +0100

    Update App.jsx
    1.0.1
    added_import_for_pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 // nextrole_landing/src/App.jsx
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/layout/Header";
 import { Footer } from "./components/layout/Footer";
-import { Hero } from "./sections/Hero";
-import { Features } from "./sections/Features";
-import { HowItWorks } from "./sections/HowItWorks";
-import { SocialProof } from "./sections/SocialProof";
-import { WhyChooseUs } from "./sections/WhyChooseUs";
-import { FAQ } from "./sections/FAQ";
-import { CTA } from "./sections/CTA";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import HowItWorks from "./sections/HowItWorks";
+import SocialProof from "./sections/SocialProof";
+import WhyChooseUs from "./sections/WhyChooseUs";
+import FAQ from "./sections/FAQ";
+import CTA from "./sections/CTA";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import Blog from "./pages/Blog";
 import "./App.css";
 
 function App() {
@@ -16,13 +22,27 @@ function App() {
     <div className="app">
       <Header />
       <main className="main-content">
-        <Hero />
-        <Features />
-        <HowItWorks />
-        <SocialProof />
-        <WhyChooseUs />
-        <FAQ />
-        <CTA />
+        <Routes>
+          {/* Homepage Route */}
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Features />
+              <HowItWorks />
+              <SocialProof />
+              <WhyChooseUs />
+              <FAQ />
+              <CTA />
+            </>
+          } />
+
+          {/* Supporting Pages */}
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
       </main>
       <Footer />
     </div>
