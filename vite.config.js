fed396cb7506c4aaee8738c3531c9e7884fc7704
Isaac Familoni <91315160+isaacfib@users.noreply.github.com>
2025-03-01 06:23:01 +0100

    Update vite.config.js
    1.0.1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Adjust if deploying to a subdirectory
+  server: {
+    port: 5173,
+    strictPort: true,
+    open: true
+  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    sourcemap: true,
-  },
+    emptyOutDir: true
+  }
 });
