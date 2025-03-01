d2659879fa3def26e3dfc5d0cd4628f42bd22643
Isaac Familoni <91315160+isaacfib@users.noreply.github.com>
2025-03-01 05:56:42 +0100

    Create About.jsx
    code_1.0.0

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+// src/pages/About.jsx
+import React from 'react';
+
+const About = () => (
+  <div className="page-container">
+    <h1>About Nextrole</h1>
+    <p>Temporary about page content</p>
+  </div>
+);
+
+export default About;
