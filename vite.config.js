62be87e9fd018e1ee98aa8fe33610b3fb66c2e01
Isaac Familoni <91315160+isaacfib@users.noreply.github.com>
2025-03-04 10:01:53 +0100

    Update vite.config.js
    Outputs directly to docs

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,19 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    open: true,
-    historyApiFallback: {
-      index: '/index.html'
-    }
-  },
-  base: '/',  // Use absolute paths for GitHub Pages
-  build: {
-    outDir: 'docs',  // Output built files to /docs
-    rollupOptions: {
-      input: {
-        main: resolve(new URL('public/index.html', import.meta.url).pathname)  // Entry point is root index.html
-      }
-    }
-  }
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    open: true,
+    historyApiFallback: {
+      index: '/index.html'
+    }
+  },
+  base: './',
+  build: {
+    outDir: 'docs',  // Output directly to 'docs' folder
+    
+  }
 });
