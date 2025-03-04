ed45a7ee713f78d75d9b7c82a3fca3f9e306679f
isaacfib <isfamiloni@gmail.com>
2025-03-04 14:36:26 +0100

    1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,11 @@
-// src/main.jsx
+// nextrole_landing/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import history from './history';
 
-// Wrap App with HistoryRouter
-import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HistoryRouter history={history}>
-      <App />
-    </HistoryRouter>
-  </React.StrictMode>
-);
-// Get the root element
-const rootElement = document.getElementById("root");
-
-// Check if element exists
-if (!rootElement) throw new Error("Root element not found");
-
-// Create root and render
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
