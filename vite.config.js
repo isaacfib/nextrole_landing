9d9a262b0c10ec36df5ed93d2d329af0acb4222c
isaacfib <isfamiloni@gmail.com>
2025-03-04 15:31:08 +0100

    updated_1.0.1

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,25 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    open: true,
-    historyApiFallback: {
-      index: '/index.html'
-    }
-  },
-  base: './',
-  build: {
-    outDir: 'docs',  // Output directly to 'docs' folder
-  }
+  plugins: [react()],
+  root: resolve(__dirname, './'), // Project root
+  publicDir: resolve(__dirname, './public'), // Public directory
+  base: './', // Relative base path
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    open: true,
+    historyApiFallback: {
+      index: '/index.html'
+    }
+  },
+  build: {
+    outDir: 'docs',  // Output to 'docs' folder
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html') // Point to index.html in public directory
+      }
+    }
+  }
 });
